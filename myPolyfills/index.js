44b82083fe2c95c4b75eb405467ea5a94f21dd39
Drimodaren <men.a.a@yandex.ru>
2022-08-12 15:42:18 +0300

    1

--- myPolyfills/index.js
+++ myPolyfills/index.js
@@ -1,1 +1,8 @@
-module.exports = function () {}
+module.exports = function () {
+  //проверим сначала отсутсвует ли вообще этот метод, среди существующих
+  if (!Array.prototype.myMap)
+  Array.prototype.myMap = function () {
+
+  }
+
+}
